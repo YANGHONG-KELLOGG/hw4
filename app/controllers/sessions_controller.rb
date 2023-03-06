68c3dc9a5fbcd4c04b9e54d4449d3b90bff62773
YANGHONG-KELLOGG <yang.hong@kellogg.northwestern.edu>
2023-03-06 23:07:39 +0000

    part 1 done

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,34 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by({"email"=>params["email"]})
+    
+    if @user
+        # if user exists
+        if BCrypt::Password.new(@user["password"]) == params["password"]
+        session["user_id"] =@user["id"]
+            # send them to places
+        flash["notice"] = "Welcome, #{@user["username"]}."
+        redirect_to "/places"
+        
+        else
+        #otherwise, back to login
+        flash["notice"] = "Password not right"
+        redirect_to "/login"
+        end
+    else
+    #otherwise, back to login
+    flash["notice"] = "No user found"
+    redirect_to "/login"
+    end 
+end 
+
+
+def destroy
+    flash["notice"] = "Goodbye."
+    session["user_id"] = nil
+    redirect_to "/login"
   end
 
-  def destroy
-  end
 end
   
